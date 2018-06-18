e719299c2f53a3705b896046840378e76a07acce
anastasiaval <36269769+anastasiaval@users.noreply.github.com>
2018-06-18 17:59:52 +0300

    configuration

---
+++ src/Homepage.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Container, Row, Col } from 'reactstrap';
+
+export default class HomePage extends Component {
+    render() {
+        return (
+            <Container>
+                <Row>
+                    <Col>
+
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
+}
---
+++ src/index.jsx
@@ -0,0 +1,18 @@
+import React, { Component, Fragment } from 'react';
+import ReactDom from 'react-dom';
+
+import HomePage from './Homepage.jsx';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+    render() {
+        return (
+            <Fragment>
+                <HomePage />
+            </Fragment>
+        );
+    }
+}
+
+ReactDom.render(<App />, document.getElementById('app'));
